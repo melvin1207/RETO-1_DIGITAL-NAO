5fd2c8d61606e2f306bb0f64bbeaac9a441ae0fa
Melvin <melsava1207@gmail.com>
2024-03-23 17:56:48 -0600

    validaciones con bootstrap terminado solo falta terminar la documentacion

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ function openNav(){
 function closeNav(){
   document.getElementById("mobile-menu").style.width = "0%";
 }
+
+(() => {
+  'use strict'
+
+  const forms = document.querySelectorAll('.needs-validation')
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
